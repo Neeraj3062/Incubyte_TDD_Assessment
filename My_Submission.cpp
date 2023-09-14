5417a0c3517b000b35dbda9241aca7b3c327c08a
Neeraj Sunil Bukane <87817072+Neeraj3062@users.noreply.github.com>
2023-09-15 01:45:50 +0530

    Created moveright and moveleft functions

--- My_Submission.cpp
+++ My_Submission.cpp
@@ -41,7 +41,139 @@ public:
             break;
         }
     }
+    void turnLeft()
+    {
+        switch (direction)
+        {
+        case 'N':
+            direction = 'W';
+            ogdir = 'W';
+            break;
+        case 'S':
+            direction = 'E';
+            ogdir = 'E';
+            break;
+        case 'E':
+            direction = 'N';
+            ogdir = 'N';
+            break;
+        case 'W':
+            direction = 'S';
+            ogdir = 'S';
+            break;
+        case 'U':
+            if (ogdir == 'E')
+            {
+                direction = 'N';
+                ogdir = 'N';
+            }
+            else if (ogdir == 'N')
+            {
+                direction = 'W';
+                ogdir = 'W';
+            }
+            else if (ogdir == 'S')
+            {
+                direction = 'E';
+                ogdir = 'E';
+            }
+            else
+            {
+                direction = 'S';
+                ogdir = 'S';
+            }
+            break;
+        case 'D':
+            if (ogdir == 'E')
+            {
+                direction = 'N';
+                ogdir = 'N';
+            }
+            else if (ogdir == 'N')
+            {
+                direction = 'W';
+                ogdir = 'W';
+            }
+            else if (ogdir == 'S')
+            {
+                direction = 'E';
+                ogdir = 'E';
+            }
+            else
+            {
+                direction = 'S';
+                ogdir = 'S';
+            }
+            break;
+        }
+    }
 
+    void turnRight()
+    {
+        switch (direction)
+        {
+        case 'N':
+            direction = 'E';
+            ogdir = 'E';
+            break;
+        case 'S':
+            direction = 'W';
+            ogdir = 'W';
+            break;
+        case 'E':
+            direction = 'S';
+            ogdir = 'S';
+            break;
+        case 'W':
+            direction = 'N';
+            ogdir = 'N';
+            break;
+        case 'U':
+            if (ogdir == 'E')
+            {
+                direction = 'S';
+                ogdir = 'S';
+            }
+            else if (ogdir == 'N')
+            {
+                direction = 'E';
+                ogdir = 'E';
+            }
+            else if (ogdir == 'S')
+            {
+                direction = 'W';
+                ogdir = 'W';
+            }
+            else
+            {
+                direction = 'N';
+                ogdir = 'N';
+            }
+            break;
+        case 'D':
+            if (ogdir == 'E')
+            {
+                direction = 'S';
+                ogdir = 'S';
+            }
+            else if (ogdir == 'N')
+            {
+                direction = 'E';
+                ogdir = 'E';
+            }
+            else if (ogdir == 'S')
+            {
+                direction = 'W';
+                ogdir = 'W';
+            }
+            else
+            {
+                direction = 'N';
+                ogdir = 'N';
+            }
+            break;
+        }
+    }
     int getX()
     {
         return x;
@@ -55,28 +187,43 @@ public:
     {
         return z;
     }
-
+    char getDirection()
+    {
+        return direction;
+    }
     void printPosition()
     {
         std::cout << "Current Position: (" << x << ", " << y << ", " << z << "), Direction: " << direction << std::endl;
     }
 };
-
 int main()
 {
+    // Create a Spacecraft object
     Spacecraft mySpacecraft(0, 0, 0, 'N', 'N');
 
-    // Test moving forward
-    mySpacecraft.moveForward();
+    mySpacecraft.turnLeft();
 
-    // Check if the Y-coordinate increases
-    if (mySpacecraft.getY() == 1)
+    // Check if the direction changes to 'W' (west)
+    if (mySpacecraft.getDirection() == 'W')
     {
-        std::cout << "Move forward test passed." << std::endl;
+        std::cout << "Turn left test passed." << std::endl;
     }
     else
     {
-        std::cout << "Move forward test failed." << std::endl;
+        std::cout << "Turn left test failed." << std::endl;
+    }
+
+
+    mySpacecraft.turnRight();
+
+    // Check if the direction changes back to 'N' (north)
+    if (mySpacecraft.getDirection() == 'N')
+    {
+        std::cout << "Turn right test passed." << std::endl;
+    }
+    else
+    {
+        std::cout << "Turn right test failed." << std::endl;
     }
     mySpacecraft.printPosition();
     return 0;
