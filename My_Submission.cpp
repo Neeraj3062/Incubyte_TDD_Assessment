34930191bb2b2dcb452b8e4b4f78a854007dba09
Neeraj Sunil Bukane <87817072+Neeraj3062@users.noreply.github.com>
2023-09-15 01:35:54 +0530

    Tested moveforward function

--- My_Submission.cpp
+++ My_Submission.cpp
@@ -1,33 +1,83 @@
 #include <iostream>
-using namespace std;
 
 class Spacecraft
 {
 private:
-    int posX, posY, posZ;  // Current position (x, y, z)
-    char currentDirection; // Current direction (N, S, E, W, U, D)
-    char originalDirection;
+    int x, y, z;    // Current position (x, y, z)
+    char direction; // Current direction (N, S, E, W, U, D)
+    char ogdir;
 
 public:
     Spacecraft(int initialX, int initialY, int initialZ, char initialDirection, char og)
     {
-        posX = initialX;
-        posY = initialY;
-        posZ = initialZ;
-        currentDirection = initialDirection;
-        originalDirection = og;
+        x = initialX;
+        y = initialY;
+        z = initialZ;
+        direction = initialDirection;
+        ogdir = og;
+    }
+
+    void moveForward()
+    {
+        switch (direction)
+        {
+        case 'N':
+            y++;
+            break;
+        case 'S':
+            y--;
+            break;
+        case 'E':
+            x++;
+            break;
+        case 'W':
+            x--;
+            break;
+        case 'U':
+            z++;
+            break;
+        case 'D':
+            z--;
+            break;
+        }
+    }
+
+    int getX()
+    {
+        return x;
+    }
+
+    int getY()
+    {
+        return y;
+    }
+    int getZ()
+    {
+        return z;
     }
 
     void printPosition()
     {
-        std::cout << "Current Position: (" << posX << ", " << posY << ", " << posZ << "), Direction: " << currentDirection << std::endl;
+        std::cout << "Current Position: (" << x << ", " << y << ", " << z << "), Direction: " << direction << std::endl;
     }
 };
 
 int main()
 {
     Spacecraft mySpacecraft(0, 0, 0, 'N', 'N');
-    mySpacecraft.printPosition();
 
+    // Test moving forward
+    mySpacecraft.moveForward();
+
+    // Check if the Y-coordinate increases
+    if (mySpacecraft.getY() == 1)
+    {
+        std::cout << "Move forward test passed." << std::endl;
+    }
+    else
+    {
+        std::cout << "Move forward test failed." << std::endl;
+    }
+    mySpacecraft.printPosition();
     return 0;
 }
